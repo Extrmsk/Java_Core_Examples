dda7f9a0c733b29b782204a7e292420800941f6b
Lemanov Egor <extrmsk@gmail.com>
2016-12-05 19:31:36 +0700

    add PrintAllCharReplaces

--- src/main/java/Strings/PrintAllCharReplaces.java
+++ src/main/java/Strings/PrintAllCharReplaces.java
@@ -7,35 +7,47 @@ import java.util.Set;
 /**
  * Created by Extr on 02.12.2016.
  */
+
+// Find all unique combinations chars in string
 public class PrintAllCharReplaces {
+
     private static Set<String> set;
     private static int count = 0;
 
     public static void main(String[] args) {
-        String str = "abcdefg";
+        String str = "aac";
         set = new HashSet<String>();
 
-        recursion("",str);
-
-        for (String s : set) {
-            System.out.println(s);
-        }
+        recursion("", str);
+        System.out.println(set);
         System.out.println("Number of iterations = " + count);
         System.out.println("Number of combinations = " + set.size());
-
     }
+
 
     static void recursion(String begin, String end) {
         if (end.length() == 2) {
             count++;
-            String str1 = begin + end.charAt(0) + end.charAt(1);
-            String str2 = begin + end.charAt(1) + end.charAt(0);
-            set.add(str1);
-            set.add(str2);
+            set.add(begin + end.charAt(0) + end.charAt(1));
+            set.add(begin + end.charAt(1) + end.charAt(0));
         } else {
-            for (Character ch : end.toCharArray()) {
-                recursion((begin + ch), end.substring(0, end.length() - 1));
+            int endLength = end.length();
+            for (int i = 0; i < endLength; i++) {
+                recursion(begin + end.charAt(i), remCharFromString(end, i));
             }
         }
+    }
+
+    static String remCharFromString(String str, int index) {
+        int strLength = str.length();
+        String result;
+        if (index == 0) {
+            result = str.substring(1);
+        } else if (index == strLength - 1) {
+            result = str.substring(0, strLength - 1);
+        } else {
+            result = str.substring(0, index) + str.substring(index + 1, strLength);
+        }
+        return result;
     }
 }
